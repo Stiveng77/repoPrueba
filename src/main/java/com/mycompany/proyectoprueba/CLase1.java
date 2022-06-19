56a507ec11ac3c9123b9716fecd68c6298c6a45f
Stiveng77 <stguevaram@unal.edu.co>
2022-06-19 04:23:04 -0500

    Commiit prueba

---
+++ src/main/java/com/mycompany/proyectoprueba/CLase1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoprueba;
+
+/**
+ *
+ * @author HP
+ */
+public class CLase1 {
+    
+    String Nombre; 
+    int edad; 
+
+    public CLase1(String Nombre, int edad) {
+        this.Nombre = Nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/proyectoprueba/mainPrueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoprueba;
+
+
+public class mainPrueba {
+    
+    public static void main(String[] args) {
+        
+        CLase1 p= new CLase1("Camilo",2);
+        
+        p.getNombre();
+        System.out.println(p.getNombre());
+        p.setNombre("Stiven");
+    }
+}
